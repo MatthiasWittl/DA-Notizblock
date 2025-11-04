fc022902c10fad67bacb4496517df20b30d3aac7
MatthiasWittl <Matthias_Wittl@outlook.de>
2025-11-04 20:23:09 +0100

    button added

---
+++ script.js
@@ -0,0 +1,21 @@
+
+let notes = ['banana', 'rasen mähen', 'Test 1', ' Test 2', 'Test 3'];
+
+function renderNotes () {
+    let contentRef = document.getElementById('content');
+    contentRef.innerHTML = "";
+    for (let indexNote = 0; indexNote < notes.length; indexNote++) {
+        const note = notes[indexNote];
+        contentRef.innerHTML += " " + notes;
+    }
+}
+
+function getNoteTemplate () {
+    return `
+         <table>
+            <th>
+                <td></td>
+            </th>
+        </table>
+            `
+}
