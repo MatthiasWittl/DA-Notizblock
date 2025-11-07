6487aeb474d3f2a4e541cf104e4039748c1a66e0
MatthiasWittl <Matthias_Wittl@outlook.de>
2025-11-07 19:15:30 +0100

    function to read and add value of the input fields added

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+let notesTitles = [];
+let notes = [];
+
+
+let finishedNotesTitles = [];
+let finishedNotes = [];
+
+let trashNotesTitles = [];
+let trashNotes = [];
+
+
+function addNote() {
+    notes.push(document.getElementById("note_input").value);
+    notesTitles.push(document.getElementById("title_input").value);
+    console.log(notesTitles, notes);
+}
