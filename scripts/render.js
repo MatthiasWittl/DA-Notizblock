dc54bd3b238477b0e83fe8ce372bdfffb5d48dbe
MatthiasWittl <Matthias_Wittl@outlook.de>
2025-11-08 20:38:27 +0100

    render notes function added + saving to local Storage

--- scripts/render.js
+++ scripts/render.js
@@ -1,5 +1,10 @@
 
 
-function renderNewNotes() {
-    return ' <div></div>'
-}
+function renderNotes(index) {
+    return `
+    <h3>${notesTitles[index]}</h3>
+    <p>${notes[index]}</p>
+    <input type="checkbox" name="" id="${index}">
+    `   
+} 
+
--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
-let notesTitles = [];
-let notes = [];
+let notesTitles = ["Test"];
+let notes = ["Test Notiz"];
 
 
 let finishedNotesTitles = [];
@@ -12,4 +12,19 @@ let trashNotes = [];
 function addNote() {
     notes.push(document.getElementById("note_input").value);
     notesTitles.push(document.getElementById("title_input").value);
+    createNotes();
+    localStorage.setItem("notes", notes);
+}
+
+function createNotes() {
+        document.getElementById("content").innerHTML = "";
+    for (let index = 0; index < notes.length; index++) {
+        document.getElementById("content").innerHTML += renderNotes(index); 
+    }
+    
+
+}
+
+function addNoteToDoneSection() {
+    
 }
