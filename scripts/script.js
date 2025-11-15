ad18d61a4d55498eb193d71b91c6732c55e65588
MatthiasWittl <Matthias_Wittl@outlook.de>
2025-11-15 12:29:04 +0100

    save to local Storage and restore after refresh added

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,3 @@
-let notesTitles = ["Test"];
-let notes = ["Test Notiz"];
-
-let finishedNotesTitles = [];
-let finishedNotes = [];
-
-let trashNotesTitles = [];
-let trashNotes = [];
 
 let notesObject = {
   sections: ["content", "done_content", "trash_content"],
@@ -18,10 +10,12 @@ let notesObject = {
 };
 
 function initializeNotesForSections() {
-  if (localStorage.getItem(notesObject[notes]) === null) {
-  } else {
-    notesObject = localStorage.getItem(notesObject);
+  if (localStorage.getItem("notesObject") !== null) {
+    notesObject = JSON.parse(localStorage.getItem("notesObject"));
     createNotes();
+  }
+    else { createNotes();
+    
   }
 };
 
@@ -32,7 +26,7 @@ function addNote() {
 };
 
 function createNotes() {
-  /*saveToLocalStorage();*/
+  saveToLocalStorage();
   notesCounter();
   for (let indexObject = 0; indexObject < notesObject["sections"].length; indexObject++) {
     document.getElementById(notesObject['sections'][indexObject]).innerHTML = "";
@@ -75,102 +69,6 @@ function notesCounter() {
       notesObject["trashNotes"].length;
 };
 
-
-/*
-function addToDoneSection(index) {
-  finishedNotes.push(notes.splice(index, 1));
-  finishedNotesTitles.push(notesTitles.splice(index, 1));
-  saveToLocalStorage();
-  createNotes();
-  createDoneNotes();
-};
-
-
-function createDoneNotes() {
-  notesCounter();
-  document.getElementById("done_content").innerHTML = "";
-  for (let index = 0; index < finishedNotes.length; index++) {
-    document.getElementById("done_content").innerHTML += renderDoneNotes(index);
-  }
-}
-
-function createTrashNotes() {
-  notesCounter();
-  document.getElementById("trash_content").innerHTML = "";
-  for (let index = 0; index < trashNotes.length; index++) {
-    document.getElementById("trash_content").innerHTML +=
-      renderTrashNotes(index);
-  }
-}
-
-
-function addToTrashSection(index) {
-  trashNotes.push(finishedNotes.splice(index, 1));
-  trashNotesTitles.push(finishedNotesTitles.splice(index, 1));
-  saveToLocalStorage();
-  createDoneNotes();
-  createTrashNotes();
-}
-
-function addDirectToTrash(index) {
-  trashNotes.push(notes.splice(index, 1));
-  trashNotesTitles.push(notesTitles.splice(index, 1));
-  saveToLocalStorage();
-  createNotes();
-  createTrashNotes();
-}
-
-function moveBackFromTrashToActive(index) {
-  notes.push(trashNotes.splice(index, 1));
-  notesTitles.push(trashNotesTitles.splice(index, 1));
-  saveToLocalStorage();
-  createNotes();
-  createTrashNotes();
-}
-
-function removeNoteComplete(index) {
-  trashNotes.splice(index, 1);
-  trashNotesTitles.splice(index, 1);
-  saveToLocalStorage();
-  createTrashNotes();
-}
-
-function fromDoneToActive(index) {
-  notes.push(finishedNotes.splice(index, 1));
-  notesTitles.push(finishedNotesTitles.splice(index, 1));
-  saveToLocalStorage();
-  createNotes();
-  createDoneNotes();
-}
-
-function fromTrashToDone(index) {
-  finishedNotes.push(trashNotes.splice(index, 1));
-  finishedNotesTitles.push(trashNotesTitles.splice(index, 1));
-  saveToLocalStorage();
-  createDoneNotes();
-  createTrashNotes();
-}
-
 function saveToLocalStorage() {
-  localStorage.setItem("notesObject", notesObject);
-  /*
-  localStorage.setItem(notes, JSON.stringify(notes));
-  localStorage.setItem("notesTitles", JSON.stringify(notesTitles));
-  localStorage.setItem("finishedNotes", JSON.stringify(finishedNotes));
-  localStorage.setItem(
-    "finishedNotesTitles",
-    JSON.stringify(finishedNotesTitles)
-  );
-  localStorage.setItem("trashNotes", JSON.stringify(trashNotes));
-  localStorage.setItem("trashNotesTitles", JSON.stringify(trashNotesTitles)); 
+    localStorage.setItem("notesObject", JSON.stringify(notesObject));
 }
-
-function getFromLocalStorage() {
-  notes = JSON.parse(localStorage.getItem("notes"));
-  notesTitles = JSON.parse(localStorage.getItem("notesTitles"));
-  finishedNotes = JSON.parse(localStorage.getItem("finishedNotes"));
-  finishedNotesTitles = JSON.parse(localStorage.getItem("finishedNotesTitles"));
-  trashNotes = JSON.parse(localStorage.getItem("trashNotes"));
-  trashNotesTitles = JSON.parse(localStorage.getItem("trashNotesTitles"));
-}
-*/
